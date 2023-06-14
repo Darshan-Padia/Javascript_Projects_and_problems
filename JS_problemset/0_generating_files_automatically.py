f4b3aed7e46815bfbbd53890748dec94cf55ca32
Darshan Padia <darshanpadia5@gmail.com>
2023-06-14 12:16:24 +0530

    .

---
+++ JS_problemset/0_generating_files_automatically.py
@@ -0,0 +1,3 @@
+# autogenerate files for problemset
+for i in range(1,51):
+    f = open(f'JS_problemset/Q{i}.js', 'x')
--- JS_problemset/any.py
+++
@@ -1,2 +0,0 @@
-for i in range(31,51):
-    f = open(f'JS_problemset/Q{i}.js', 'x')
